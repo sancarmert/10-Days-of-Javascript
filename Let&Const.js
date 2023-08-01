fe81512cb2af2d28e6e9d2511d64bc91f41b0a9c
Mert Sancar <msmertsancar@gmail.com>
2023-08-01 10:44:24 +0300

    first commit

---
+++ Let&Const.js
@@ -0,0 +1,10 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+const PI = Math.PI;
+let r = readLine();
+let area = PI * r * r;
+let perimeter = 2 * PI * r;
+// Print the area of the circle:
+// Print the perimeter of the circle:
+console.log(area + "\n" + perimeter); 
+}
