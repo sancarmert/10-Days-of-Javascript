321639800c46d93fb521b473d84ad5d95ab4faf7
Mert Sancar <msmertsancar@gmail.com>
2023-08-19 14:50:29 +0300

    first commit

---
+++ Bitwise_Operators.js
@@ -0,0 +1,10 @@
+function getMaxLessThanK(n, k) {
+    let max=0;
+    for(let a = 1; a <= n; a++)
+        for(let b = a + 1; b <= n; b++) {
+            let x = a & b;
+            if (max < x &&  x < k)
+                    max = x;
+        }
+        return max;
+}
