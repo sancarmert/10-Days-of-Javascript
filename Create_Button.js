339950fe2bdf9ed8c0b9687f8a6f13357c64abe2
Mert Sancar <msmertsancar@gmail.com>
2023-09-05 11:10:50 +0300

    firt commit

---
+++ Create_Button.js
@@ -0,0 +1,26 @@
+/* index.html
+<!DOCTYPE html> 
+<html>     
+  <head>         
+     <meta charset="utf-8">
+     <title>Button</title>
+     <link rel="stylesheet" href="css/button.css" type="text/css">   
+  </head>       
+   <body> 
+     <button id="btn" type="button">0</button>
+     <script src="js/button.js" type="text/javascript"></script> 
+   </body> 
+</html>
+*/
+/* button.css
+#btn{
+    width:96px;
+    height:48px;
+    font-size:24px;
+}
+*/
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('btn');
+button.addEventListener('click', (e) => {
+          const count = Number(e.currentTarget.innerText) + 1;
+      e.currentTarget.innerText = count;  }); });
